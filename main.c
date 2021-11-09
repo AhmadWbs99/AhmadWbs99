377ecb4a7fcb4026befeb5ab626b953256f7608c
Ahmad <ahmad.weabeusar@gmail.com>
2021-11-09 21:13:31 +0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    printf(" Student ID : 641431020 \n");
+    printf(" Name is :Ahmad waebeusar \n");
+    printf(" Address : Patani ,Yaring \n"); 
+    printf(" Email : ahmad.weabeusar@gmail.com \n");
+    printf(" Mobile Number : 0612498690 \n");
+    
+
+    return 0;
+}
