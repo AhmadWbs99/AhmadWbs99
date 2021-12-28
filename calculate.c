97ee8910ad3ea560f74cdbfcec2f45f34e3b47e6
Ahmad Waebeusar <88262130+AhmadWbs99@users.noreply.github.com>
2021-12-28 20:28:39 +0700

    Add files via upload
    Code for take a value From User

---
+++ calculate.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main (){
+
+int Num1 , Num2;
+printf("Enter num1 : ");
+scanf("%d",&Num1);
+printf("Enter Num2 : ");
+scanf("%d",&Num2);
+printf("The Num1 is: %d , The Num2 is : %d ",Num1,Num2);
+
+
+
+
+return 0;
+}
